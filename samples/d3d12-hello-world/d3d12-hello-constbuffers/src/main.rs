36d7084ada4b6a6bd8d2f303161decc16567f8f7
Damyan Pepper <damyanp@microsoft.com>
2022-11-28 13:12:43 -0800

    Remove transmutes in favor of safer alternatives

--- samples/d3d12-hello-world/d3d12-hello-constbuffers/src/main.rs
+++ samples/d3d12-hello-world/d3d12-hello-constbuffers/src/main.rs
@@ -13,8 +13,6 @@ extern crate static_assertions as sa;
 
 mod d3d12_hello_constbuffers {
 
-    use std::intrinsics::transmute;
-
     use super::*;
 
     const FRAME_COUNT: usize = 2;
@@ -384,7 +382,7 @@ mod d3d12_hello_constbuffers {
             Anonymous: D3D12_VERSIONED_ROOT_SIGNATURE_DESC_0 {
                 Desc_1_1: D3D12_ROOT_SIGNATURE_DESC1 {
                     NumParameters: 1,
-                    pParameters: unsafe { transmute(&root_parameters) },
+                    pParameters: root_parameters.as_ptr() as _,
                     Flags: D3D12_ROOT_SIGNATURE_FLAG_ALLOW_INPUT_ASSEMBLER_INPUT_LAYOUT,
                     ..Default::default()
                 },
